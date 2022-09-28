65635d0daa12ef606098dacf7f4d8bc24cd951f2
tosmel2 <adewaletosin0808@gmail.com>
2022-09-28 17:16:58 +0100

    2022 Explorer landing page, Built in Xeraxlabs Internship.

---
+++ DX-Explorer/script.js
@@ -0,0 +1,18 @@
+// Mobile responsive menu
+
+const backdrop = document.querySelector(".backdrop");
+const menu = document.getElementById("barbox");
+
+function controller() {
+    document.querySelector(".nav__Mobilelinks").classList.toggle("show");
+    backdrop.classList.toggle("show");
+    menu.classList.toggle("change")
+}
+
+menu.addEventListener("click", function () {
+    controller();
+})
+
+backdrop.addEventListener('click', function () {
+    controller();
+})
